e9b5666bb195896c6bac4d3af8f34701dfb240d6
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-09-22 16:18:45 -0500

    Add invoke scripts

---
+++ sharepoint/InvokeSync-KRCLogistics.ps1
@@ -0,0 +1,10 @@
+﻿$ScriptUrl="https://raw.githubusercontent.com/jwaltzjr/krc_intune/master/sharepoint/Sync-KRCLogistics.ps1"
+$ScriptName="SharepointSync-KRCLogistics"
+
+$psCommand= "PowerShell.exe -ExecutionPolicy Bypass -windowstyle hidden -command $([char]34)& {(Invoke-WebRequest $ScriptUrl).Content | Invoke-Expression}$([char]34)"
+$regKeyLocation="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+if (!(Test-Path -Path $regKeyLocation)){
+    New-ItemProperty -Path $regKeyLocation -Force
+}
+Set-ItemProperty -Path $regKeyLocation -Name $ScriptName -Value $psCommand -Force
+Invoke-Expression $psCommand
