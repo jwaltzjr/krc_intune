05c3462f0c3e0f2301f2c66d4b84736e8ea40c07
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-09-22 16:19:17 -0500

    Fixed reg check and added debugging

--- InstallCitrix.ps1
+++ InstallCitrix.ps1
@@ -2,11 +2,15 @@ $Url = "https://downloadplugins.citrix.com/Windows/CitrixWorkspaceApp.exe"
 $Target = "$env:SystemRoot\Temp\CitrixWorkspaceApp.exe"
 $Arguments = 'DONOTSTARTCC=1 /AutoUpdateCheck=Auto /AutoUpdateStream=LTSR /DeferUpdateCount=3 /AURolloutPriority=Slow /NoReboot /Silent EnableCEIP=False'
 
-if (!(Test-Path -Path HKEY_CURRENT_USER\SOFTWARE\Citrix\ICA_Client)) {
+if (!(Test-Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Citrix\PluginPackages\XenAppSuite\ICA_Client)) {
+    Write-Host "Citrix not installed - downloading"
     Start-BitsTransfer -Source $Url -Destination $Target -Priority High -TransferPolicy Always -ErrorAction Continue
     $t = Start-Process -FilePath $Target -ArgumentList $Arguments -PassThru -ErrorAction Stop
     if($t -ne $null)
     {
        Wait-Process -InputObject $t
     }
+}
+else {
+    Write-Host "Citrix installed"
 }
