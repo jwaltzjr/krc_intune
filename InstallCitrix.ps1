5882178f51acca4efa39b37c2faab74dfd7043f2
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-09-22 13:04:18 -0500

    Add citrix install script

---
+++ InstallCitrix.ps1
@@ -0,0 +1,12 @@
+﻿$Url = "https://downloads.citrix.com/18632/CitrixWorkspaceApp.exe?__gda__=1600745047_0e65890719326c138dbfbbee451186b5"
+$Target = "$env:SystemRoot\Temp\CitrixWorkspaceApp.exe"
+$Arguments = 'DONOTSTARTCC=1 /AutoUpdateCheck=Auto /AutoUpdateStream=LTSR /DeferUpdateCount=3 /AURolloutPriority=Slow /NoReboot /Silent EnableCEIP=False'
+
+if (!(Test-Path -Path HKEY_CURRENT_USER\SOFTWARE\Citrix\ICA_Client)) {
+    Start-BitsTransfer -Source $Url -Destination $Target -Priority High -TransferPolicy Always -ErrorAction Continue
+    $t = Start-Process -FilePath $Target -ArgumentList $Arguments -PassThru -ErrorAction Stop
+    if($t -ne $null)
+    {
+       Wait-Process -InputObject $t
+    }
+}
