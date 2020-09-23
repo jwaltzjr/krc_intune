bab53775724d382a5e8dd8364cd3233b2450b29b
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-09-23 01:21:47 -0500

    Remove key if installed

--- InstallCitrix.ps1
+++ InstallCitrix.ps1
@@ -1,6 +1,8 @@
 $Url = "https://downloadplugins.citrix.com/Windows/CitrixWorkspaceApp.exe"
 $Target = "$env:SystemRoot\Temp\CitrixWorkspaceApp.exe"
 $Arguments = 'DONOTSTARTCC=1 /AutoUpdateCheck=Auto /AutoUpdateStream=LTSR /DeferUpdateCount=3 /AURolloutPriority=Slow /NoReboot /Silent EnableCEIP=False'
+$ScriptName="Install Citrix"
+$regKeyLocation="HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
 
 if (!(Test-Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Citrix\PluginPackages\XenAppSuite\ICA_Client)) {
     Write-Host "Citrix not installed - downloading"
@@ -13,4 +15,5 @@ if (!(Test-Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Citrix\PluginP
 }
 else {
     Write-Host "Citrix installed"
+    Remove-ItemProperty -Path $regKeyLocation -Name $ScriptName -Force
 }
