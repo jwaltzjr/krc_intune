8a585db7e469d04f60748441a8888abd8140c50a
Jeff Waltz Jr <jwaltzjr@krclogistics.com>
2020-09-22 20:08:43 -0500

    Change to computer acct and dont use ie

--- InvokeInstallCitrix.ps1
+++ InvokeInstallCitrix.ps1
@@ -1,8 +1,8 @@
 ﻿$ScriptUrl="https://raw.githubusercontent.com/jwaltzjr/krc_intune/master/InstallCitrix.ps1"
 $ScriptName="Install Citrix"
 
-$psCommand= "PowerShell.exe -ExecutionPolicy Bypass -windowstyle hidden -command $([char]34)& {(Invoke-WebRequest $ScriptUrl).Content | Invoke-Expression}$([char]34)"
-$regKeyLocation="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$psCommand= "PowerShell.exe -ExecutionPolicy Bypass -windowstyle hidden -command $([char]34)& {(Invoke-WebRequest $ScriptUrl -UseBasicParsing).Content | Invoke-Expression}$([char]34)"
+$regKeyLocation="HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
 if (!(Test-Path -Path $regKeyLocation)){
     New-ItemProperty -Path $regKeyLocation -Force
 }
